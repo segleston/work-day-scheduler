0179e5382ecec7dce8f6a7a638f9e65571e53dcd
segleston <suraheg@gmail.com>
2024-01-05 19:42:46 +0000

    added pseudocode to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,30 @@ $(document).ready(function () {
 
 
     function displayTime() {
+
+        // adds text for current day
         $('#currentDay').text(dayjs().format
             ('dddd, MMMM D, YYYY'))
         $('#currentTime').text(dayjs().format
         ('h:mm:ss'))
     }
 
+    // interval to display the time and update every second
     setInterval(displayTime, 1000)
 
     function updateHour() {
+        // current hour of the day
         let currentHour = dayjs().hour()
         console.log(currentHour)
+        // grabs class for the time blocks and stores in timeBlocks var
         let timeBlocks = $('.time-block')
         console.log(timeBlocks)
 
         timeBlocks.each(function() {
+            // parse the id for each time block and compare the block hour to the current time
             let blockHour = parseInt($(this).attr('id'))
             console.log(blockHour)
-            
+            // adds specific class based on if its past/future/present
             if (currentHour > blockHour) {
                 $(this).addClass('past')
             } else if(currentHour === blockHour) {
@@ -34,9 +40,9 @@ $(document).ready(function () {
 
         })
     }
-
+    // Calls the update hour function
     updateHour()
-
+    // interval to update every hour
     setInterval(updateHour, 15000)
 
 
